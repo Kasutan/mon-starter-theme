115d61eaa95de485b1ca35e268a6360c093aa468
Kasutan <contact@kasutan.pro>
2019-05-18 14:15:48 +0200

    Gestion de la sidebar

--- page-sidebar.php
+++ page-sidebar.php
@@ -1,16 +1,7 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Kasutan
- */
+* Template Name: Avec sidebar
+*/
 
 get_header();
 ?>
@@ -36,4 +27,5 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
+get_sidebar();
 get_footer();
