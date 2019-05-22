a22e9d56715e838c78c59d952e4921e36dc51db2
Kasutan <contact@kasutan.pro>
2019-05-22 19:30:06 +0200

    Snippets

---
+++ snippets/script-plugins-local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wp plugin --allow-root install autoptimize
+wp plugin --allow-root install block-bad-queries
+wp plugin --allow-root install caldera-forms
+wp plugin --allow-root install duplicate-post
+wp plugin --allow-root install fast-page-switch
+wp plugin --allow-root install format-media-titles
+wp plugin --allow-root install imsanity
+wp plugin --allow-root install updraftplus
+wp plugin --allow-root install wp-smushit
+wp plugin --allow-root install wp-seopress
+wp plugin --allow-root install secupress
+wp plugin --allow-root install gutenberg
+wp plugin --allow-root install polylang
+wp plugin --allow-root install gutenberg
+wp plugin --allow-root install companion-auto-update
+wp plugin --allow-root install advanced-custom-fields
+wp plugin --allow-root install wp-pagenavi
+wp plugin --allow-root activate --all
---
+++ snippets/script-plugins-serveur.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+php wp-cli.phar plugin install autoptimize
+php wp-cli.phar plugin install block-bad-queries
+php wp-cli.phar plugin install caldera-forms
+php wp-cli.phar plugin install duplicate-post
+php wp-cli.phar plugin install fast-page-switch
+php wp-cli.phar plugin install format-media-titles
+php wp-cli.phar plugin install imsanity
+php wp-cli.phar plugin install updraftplus
+php wp-cli.phar plugin install wp-smushit
+php wp-cli.phar plugin install wp-seopress
+php wp-cli.phar plugin install secupress
+php wp-cli.phar plugin install gutenberg
+php wp-cli.phar plugin install polylang
+php wp-cli.phar plugin install gutenberg
+php wp-cli.phar plugin install companion-auto-update
+php wp-cli.phar plugin install advanced-custom-fields
+php wp-cli.phar plugin install wp-pagenavi
+php wp-cli.phar plugin activate --all
