10793b13df125caab15b6339d7fc1e2af64811fd
Kasutan <contact@kasutan.pro>
2019-05-22 22:29:05 +0200

    Animations

--- functions.php
+++ functions.php
@@ -210,6 +210,9 @@ function kasutan_scripts() {
 
 	wp_enqueue_script( 'kasutan-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'kasutan-scripts', get_template_directory_uri() . '/js/kasutan.js', array('jquery'), '20190522', true );
+
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
---
+++ js/kasutan.js
@@ -0,0 +1,55 @@
+(function($) {
+	/*Exemple de script javascript*/
+	/*$("#home-contact").on('click', function(e) { 
+		e.preventDefault();   
+		$('#home-contact-form').slideToggle('slow');
+	});*/
+
+	/*--------------------------------------------------------------
+	# Animations
+	--------------------------------------------------------------*/
+	/*https://alligator.io/js/intersection-observer*/
+
+
+	const config = {
+		rootMargin: '50px 20px 75px 30px',
+		//threshold: [0, 0.25, 0.75, 1]
+		};
+
+		
+	observer = new IntersectionObserver((entries) => {
+		entries.forEach(entry => {
+			if (entry.intersectionRatio > 0) {
+			entry.target.classList.add('fancy');
+			observer.unobserve(entry.target);
+			} else {
+			entry.target.classList.remove('fancy');
+			}
+		}, config);
+	});
+
+	const fancyElements=document.querySelectorAll('.js-animate-on-visible');
+	fancyElements.forEach(elem => {
+		observer.observe(elem);
+	});
+
+
+	observer2 = new IntersectionObserver((entries) => {
+		entries.forEach(entry => {
+			if (entry.intersectionRatio > 0) {
+			$(entry.target).children().addClass('fancy-cascade');
+			observer.unobserve(entry.target);
+			} else {
+			$(entry.target).children().removeClass('fancy-cascade');
+			}
+		}, config);
+	});
+
+	const fancyElementsCascade=document.querySelectorAll('.js-animate-on-visible-cascade');
+	fancyElementsCascade.forEach(item => {
+		observer2.observe(item);
+	});
+
+
+})( jQuery );
+
