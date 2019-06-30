1bd57e7a55e5b797936464f3655bb3e030afcf05
Kasutan <contact@kasutan.pro>
2019-06-30 18:02:59 +0200

    Animations : détecter si intersection observer est supporté

--- js/kasutan.js
+++ js/kasutan.js
@@ -10,45 +10,52 @@
 	--------------------------------------------------------------*/
 	/*https://alligator.io/js/intersection-observer*/
 
+	//Only Use the IntersectionObserver if it is supported
+	if ('IntersectionObserver' in window) {
+		const config = {
+			rootMargin: '50px 20px 75px 30px',
+			//threshold: [0, 0.25, 0.75, 1]
+			};
 
-	const config = {
-		rootMargin: '50px 20px 75px 30px',
-		//threshold: [0, 0.25, 0.75, 1]
-		};
+			
+		observer = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				if (entry.intersectionRatio > 0) {
+				entry.target.classList.add('fancy');
+				observer.unobserve(entry.target);
+				} else {
+				entry.target.classList.remove('fancy');
+				}
+			}, config);
+		});
 
-		
-	observer = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-			if (entry.intersectionRatio > 0) {
-			entry.target.classList.add('fancy');
-			observer.unobserve(entry.target);
-			} else {
-			entry.target.classList.remove('fancy');
-			}
-		}, config);
-	});
-
-	const fancyElements=document.querySelectorAll('.js-animate-on-visible');
-	fancyElements.forEach(elem => {
-		observer.observe(elem);
-	});
+		const fancyElements=document.querySelectorAll('.js-animate-on-visible');
+		fancyElements.forEach(elem => {
+			observer.observe(elem);
+		});
 
 
-	observer2 = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-			if (entry.intersectionRatio > 0) {
-			$(entry.target).children().addClass('fancy-cascade');
-			observer.unobserve(entry.target);
-			} else {
-			$(entry.target).children().removeClass('fancy-cascade');
-			}
-		}, config);
-	});
+		observer2 = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				if (entry.intersectionRatio > 0) {
+				$(entry.target).children().addClass('fancy-cascade');
+				observer.unobserve(entry.target);
+				} else {
+				$(entry.target).children().removeClass('fancy-cascade');
+				}
+			}, config);
+		});
 
-	const fancyElementsCascade=document.querySelectorAll('.js-animate-on-visible-cascade');
-	fancyElementsCascade.forEach(item => {
-		observer2.observe(item);
-	});
+		const fancyElementsCascade=document.querySelectorAll('.js-animate-on-visible-cascade');
+		fancyElementsCascade.forEach(item => {
+			observer2.observe(item);
+		});
+
+	} else {
+		//if Intersection Observer is not supported, add classes right away
+		$('.js-animate-on-visible-cascade').addClass('fancy-cascade');
+		$('.js-animate-on-visible').addClass('fancy');
+	}
 
 
 })( jQuery );
